7ec26935abe3f25c80db2bfc75fb8c96c58db1b7
Murzbul <natanaelrusso18@hotmail.com>
2018-08-09 19:38:29 -0300

    Add calculateTeamPoints functionality

--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -4,5 +4,33 @@
 // input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
 // output: [{ team: 'green', points: 40 }, ...]
 module.exports = (players) => {
-  return []
+  var res = [];
+  var teams = [];
+
+  for ( player of players )
+  {
+    if ( teams.indexOf( player.team ) == -1 )
+    {
+      teams.push( player.team );
+
+      let result = { team: player.team, points: 0 }
+
+      if( player.isActive )
+      {
+        result.points = player.points;
+      }
+
+      res[player.team] = result;
+    }
+    else
+    {
+      if( player.isActive )
+      {
+        res[player.team].points = res[player.team].points + player.points;
+      }
+    }
+  }
+  }
+
+  return res;
 }
